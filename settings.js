2eba84c61dd6a90c4f6744bddeea6b05921ac034
nicoooooqwp230 <mingoianicolo3@gmail.com>
2025-06-05 21:13:03 +0200

    nico

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "nico Bot",
   botOwner: 'nico', // Your name
-  ownerNumber: '3533182048', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '393533182048', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
